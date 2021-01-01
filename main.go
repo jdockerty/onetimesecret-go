ca76aad99e8a76fb333b5a15ca1ab6a3fae750f2
Jack Dockerty <jdockerty19@gmail.com>
2021-01-01 09:07:06 +0000

    Adding sample test code to main for package testing.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jdockerty/onetimesecret-go/ots"
+)
+
+func main() {
+	var c ots.Client
+
+	client := c.New(os.Getenv("OTS_EMAIL"), os.Getenv("OTS_KEY"))
+	client.Status()
+
+	err := client.Create("super_secret_text", "password1234", "vehis83986@cocyo.com", 60)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
