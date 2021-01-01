c6b39405c9eda4a6a9834e15636c911deaa2c2c4
Jack Dockerty <jdockerty19@gmail.com>
2021-01-01 23:35:32 +0000

    Updating main test code.

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ func main() {
 	client := c.New(os.Getenv("OTS_EMAIL"), os.Getenv("OTS_KEY"))
 	client.Status()
 
-
-	val, err := client.Generate("1234", "vehis83986@cocyo.com", 20)
+	val, err := client.Create("burn_test", "1234", "vehis83986@cocyo.com", 2000)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Printf("%+v\n", val)
 
+	// val, err := client.Burn("dv2ypby01n8qygj2qzpluhbq0yb88mf")
+	// if err != nil {
+	// 	fmt.Println("ERROR:", err)
+	// }
+	// fmt.Printf("B: %+v\n", val)
 }
